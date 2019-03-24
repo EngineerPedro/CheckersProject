7e70c165669c7a8830f7c1a5deb11a9405eb6ea4
plguzman1993 <plguzman1993@gmail.com>
2019-03-24 16:22:53 -0400

    errors debugged

--- AIfunctions.cpp
+++ AIfunctions.cpp
@@ -1,39 +1,30 @@
-#include <cstdlib>
-#include <bitset>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <limits>
-#include <cstdint>
+
 #include "AIfunctions.h"
-
-#define INFINITE 30000
-
 
 AIfunctions :: AIfunctions(){};
 
 AIfunctions :: ~AIfunctions(){};
 
-int AIfunctions :: Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Player,int Node_Index,int values[][])
+int AIfunctions :: Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Player,int Node_Index,int values[8][8])
 {
   //Terminating condition
   //leaf node is reached 
-  cout<<"Checking if depth is set to MAX_DEPTH "<endl;
+  cout<<"Checking if depth is set to MAX_DEPTH "<<endl;
   if (depth == MAX_DEPTH )/*if node is a leaf return it's value*/
   {
     return values [Node_Index] [Node_Index];
   }
   
-  cout<<"Check_Point_1"<<endl
+  cout<<"Check_Point_1"<<endl;
   if (Maximizing_Player)
   {
   int bestVal = -MAX_DEPTH;
     
-     cout<<"Check_Point_2 begining Pruning"<<endl
+     cout<<"Check_Point_2 begining Pruning"<<endl;
     //Recur for left and right child/leaf
     for (int i = 0; i < MAX_DEPTH ;i++ )
     {
-      int val = Alpha_Beta(alpha,beta,depth+1,false,Node_Index*2+1,values[][]);
+      int val = Alpha_Beta(alpha,beta,depth+1,false,Node_Index*2+1,values);
       bestVal = max(bestVal,val);
       alpha  = max(alpha,bestVal);
       
@@ -41,7 +32,7 @@ int AIfunctions :: Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Pla
         if (beta <= alpha)
           break;
     }
-    cout<<"Check_Point_4 breaking out returning value "<<endl
+    cout<<"Check_Point_4 breaking out returning value "<<endl;
     return bestVal;
   }
   else
@@ -49,18 +40,18 @@ int AIfunctions :: Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Pla
    int bestVal = MAX_DEPTH; 
     
     //Recur for left and right leaf/child
-    cout<<"Check_Point_5 going into pruning "<<endl
+    cout<<"Check_Point_5 going into pruning "<<endl;
       for (int j =0;j<MAX_DEPTH;j++)
       {
-        int val = Alpha_Beta(alpha,beta,depth+1,true,Node_Index*2+1,values[][]);
-        bestVal = miin(bestVal,val);
+        int val = Alpha_Beta(alpha,beta,depth+1,true,Node_Index*2+1,values);
+        bestVal = min(bestVal,val);
         beta = min(beta, bestVal);
         
         //Alpha Beta Pruning
         if (beta <= alpha)
           break;
       }
-    cout<<"Check_Point_6:breaking out returning value "<<endl
+    cout<<"Check_Point_6:breaking out returning value "<<endl;
     return bestVal;
   }
 }
@@ -80,18 +71,18 @@ bool AIfunctions :: DLS(int src, int target,int limit)
   for (auto i = adj[src].begin(); i != adj[src].end(); ++i)
   {
     if (DLS(*i,target,limit - 1)==true)
-      return true
+      return true;
   }
   return true;
 }
 
 //Iterative_Deepening_DFS to search if target is reachablefrom v.
 //it uses recursive DFSUtil().
-bool AIfunctions :: Iterative_Deepening_DFS(int src, int target,int max_depth)
+bool AIfunctions:: Iterative_Deepening_DFS(int src, int target,int max_depth)
 {
 //bool IDDFS(src, target, max_depth)
-int max_depth = -INFINITE; 
-  for(int limit = 0 ; limit <= max_depth ; limit++)
+int maxdepth = -MAX_DEPTH; 
+  for(int limit = 0 ; limit <= maxdepth ; limit++)
   {//foreach adjacent i or src 
     if (DLS(src,target, limit) == true)   
       //if DLS (i, target,limit?1)
--- AIfunctions.h
+++ AIfunctions.h
@@ -1,23 +1,34 @@
-#ifndef AIFunctions_H
-#define AIFunctions_H
+#ifndef AIfunctions_H
+#define AIfunctions_H
 #include <vector>
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <bitset>
+#include <vector>
+#include <limits>
+#include <bits/stdc++.h>
+
+#define INFINITE 30000
 
 #include "ply.h"
+
+using namespace std;
+
 const int MAX_DEPTH = 20;
 
+list <int> *adj;
 
-class  AIFunctions
+
+class  AIfunctions
 {
   public :
-  AIFunctions();
-  ~AIFunctions();
+  AIfunctions();
+  ~AIfunctions();
   
   //AI Functions 
-  int DLS(int src, int target,int limit);
-  int Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Player,int Node_Index,int values[][]);
-  int Iterative_Deepening_DFS(int src, int target,int max_depth);
+  bool DLS(int src, int target,int limit);
+  int Alpha_Beta(int alpha, int beta, int depth,bool Maximizing_Player,int Node_Index,int values[8][8]);
+  bool Iterative_Deepening_DFS(int src, int target,int max_depth);
 };
 #endif
